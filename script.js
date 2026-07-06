5c20d41ef3d78cce6fc635bc4b9dcfd0d32c7254
syshreya000 <sy6486698@gmail.com>
2026-07-06 08:59:04 +0530

    Add files via upload
    A modern and responsive personal portfolio website built using HTML, CSS, and JavaScript. It showcases my skills, projects, resume, and contact information with a clean UI, smooth scrolling, responsive layout, and interactive animations for an engaging user experience.

---
+++ script.js
@@ -0,0 +1,53 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 120;
+
+        if (pageYOffset >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+     });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
+const cards = document.querySelectorAll(".project-card,.skill-box span");
+
+const observer = new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+
+            entry.target.style.opacity = "1";
+            entry.target.style.transform = "translateY(0)";
+
+        }
+
+    });
+
+});
+
+cards.forEach(card => {
+
+    card.style.opacity = "0";
+    card.style.transform = "translateY(40px)";
+    card.style.transition = "0.6s";
+
+    observer.observe(card);
+
+});
